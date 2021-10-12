f7ffd6949b6c98c1cdc447634491e0619f5d7048
ffarenn <82323789+ffarenn@users.noreply.github.com>
2021-10-12 20:55:09 +0000

    Deploying to gh-pages from @ ffarenn/goit-js-hw-11-timer@50dd4bee7ea868063523b002a9552a54c07aa2da 🚀

--- src.37fe9c62.js
+++ src.37fe9c62.js
@@ -7,4 +7,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{"./refs":"VyiV"}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/timer.js");
 },{"./sass/main.scss":"clu1","./js/timer.js":"yCUT"}]},{},["Focm"], null)
-//# sourceMappingURL=/goit-js-hw-11-timer/src.3056e8ef.js.map
+//# sourceMappingURL=/goit-js-hw-11-timer/src.37fe9c62.js.map
