c97edeebd92fd11d1b7c52e1deefceb071d8cd8d
Andrejs Klimza <andrejsklimza@gmail.com>
2021-10-12 21:54:11 +0100

    adds refs export

---
+++ src/js/refs.js
@@ -0,0 +1,6 @@
+export default {
+    daysC: document.querySelector('[data-value="days"]'),
+    hoursC: document.querySelector('[data-value="hours"]'),
+    minsC: document.querySelector('[data-value="mins"]'),
+    secsC: document.querySelector('[data-value="secs"]'),
+}
